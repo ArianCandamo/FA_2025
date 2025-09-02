0f68b414999ec6e4e1465224d139739aa38699a9
arian <ariancandamo@gmail.com>
2025-09-02 15:31:17 -0500

    subiendo ejercicio1-SEM1

---
+++ SEMANA1_C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SEMANA1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejer1();
+            Console.ReadKey();
+        }
+
+        static void Ejer1()
+        {
+            string nombre,carrera; //declarando variables
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido a FA de {carrera}");
+
+        }
+
+       
+
+    }
