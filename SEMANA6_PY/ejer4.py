ef425f22a20f85f4c7b3b161dfb5b5d4822c8f8e
arian <ariancandamo@gmail.com>
2025-09-30 14:50:52 -0500

    WAZAA

---
+++ SEMANA6_PY/ejer4.py
@@ -0,0 +1,21 @@
+g = input("genere una contraseña: ")
+
+print("\n------------------------------------------------")
+print("|             Válida tu contraseña             |")
+print("|                                              |")
+print("| 1. Ud. tiene 3 intentos                      |")
+print("------------------------------------------------\n")
+
+intentos = 3
+
+while(intentos>0):
+    c = input("Ingrese la contraseña: ")
+
+    if g == c:
+        print("\nAcceso concedido. Bienvenido al sistema.")
+        break
+    else:
+        intentos-=1
+        print("Contraseña incorrecta. Intentos restantes", intentos)
+        print("Acceso denegado! cerrando sistema.")
+
